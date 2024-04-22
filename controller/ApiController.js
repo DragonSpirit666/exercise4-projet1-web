b0b962ade92abb39854d29699a537803b6c50a6e
V0rtek <2266308@cstjean.qc.ca>
2024-04-22 17:35:04 -0400

    g

--- controller/ApiController.js
+++ controller/ApiController.js
@@ -1,9 +1,22 @@
+const charactersData = require('../data');
+
 module.exports.index = (req, res) => {
-  res.json({ msg: "get", query: req.query });
+  // res.json({ msg: "get", query: req.query });
+  res.json(charactersData);
 };
 
 module.exports.store = (req, res) => {
   res.json({ msg: "post", body: req.body });
+
+  const { name, realname } = req.body;
+  if (!name) {
+    res.status(400).json({ error: 'Le nom est requis' });
+  } else {
+    const id = charactersData.length + 1;
+    const newCharacter = { id, name, realname };
+    charactersData.push(newCharacter);
+    res.status(201).json(newCharacter);
+  }
 };
 
   module.exports.update = (req, res) => {
@@ -13,6 +26,14 @@ module.exports.store = (req, res) => {
 
   module.exports.show = (req, res) => {
     res.json({ msg: "post", body: req.body });
+
+    const id = parseInt(req.params.id);
+    const character = charactersData.find(char => char.id === id);
+    if (!character) {
+      res.status(404).json({ error: 'Personnage pas trouvé' });
+    } else {
+      res.json(character);
+    }
   };
 
   module.exports.delete = (req, res) => {
